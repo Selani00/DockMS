92c9bdeac8ddba59c10db154283d9e54a4821c3c
Selani00 <pkselani00@gmail.com>
2024-06-29 10:44:09 +0530

    Add some change to test

--- front_end_/src/components/Home.js
+++ front_end_/src/components/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
             <h1>Welcome</h1>
           </div>
           <div className="bottom-heading">
-            <h2>Document Management System</h2>
+            <h2>Document Management System Test</h2>
           </div>
           <div className="Para">
             <p>
